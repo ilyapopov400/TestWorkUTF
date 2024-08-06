7d5f05ca720475b9b55b9bb384462fed153bb9aa
ilya_popov <il_pop@mail.ru>
2024-08-06 13:56:36 +0300

    settings.py && index.html for app

--- utf/app/views.py
+++ utf/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+
+class Index(TemplateView):
+    """
+    - стартовая страница приложения
+    """
+    template_name = "app/index.html"
